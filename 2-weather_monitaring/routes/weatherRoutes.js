5bf1778d3a05a8dbce30631b72b13efd4ce01546
ra318 <108418167+ra318@users.noreply.github.com>
2024-10-24 08:40:19 +0530

    Delete 2-weather_monitaring directory

--- 2-weather_monitaring/routes/weatherRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { fetchWeatherHandler } = require('../controllers/weatherController');
-
-const router = express.Router();
-
-// Route to fetch weather data
-router.post('/fetch', fetchWeatherHandler);
-
-module.exports = router;
